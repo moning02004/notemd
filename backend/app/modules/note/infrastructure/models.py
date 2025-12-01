240fd9ca8866732cc973107ed18e9986ebdf480a
jeonghoonyu <moning02004@naver.com>
2025-12-01 21:07:55 +0900

    feat: backend 모델 설계

--- backend/app/modules/note/infrastructure/models.py
+++ backend/app/modules/note/infrastructure/models.py
@@ -1,5 +1,23 @@
 from fastapi_clean_archi.core.db.base import BaseModel
+from sqlalchemy import Column, String, Text, ForeignKey, Integer, Boolean
+from sqlalchemy.orm import relationship
 
 
 class Note(BaseModel):
-    pass
+    group_id = Column(Integer, ForeignKey("group.id"), nullable=True)
+    owner_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    title = Column(String)
+    content = Column(Text)
+    is_public = Column(Boolean, default=False)
+    edit_permission = Column(Integer, default=100)  # e.g., owner, group_members
+
+    # relationships
+    owner = relationship("User", back_populates="notes")
+    group = relationship("Group", back_populates="notes")
+
+
+class Template(BaseModel):
+    name = Column(String)
+    title = Column(String)
+    content = Column(Text)
--- backend/app/modules/user/infrastructure/models.py
+++ backend/app/modules/user/infrastructure/models.py
@@ -1,5 +1,36 @@
 from fastapi_clean_archi.core.db.base import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Group(BaseModel):
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+
+    # relationships
+    users = relationship(
+        "User",
+        secondary="groupmember",
+        back_populates="groups"
+    )
+    notes = relationship("Note", back_populates="group")
 
 
 class User(BaseModel):
-    pass
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+
+    # relationships
+    groups = relationship(
+        "Group",
+        secondary="user_group",
+        back_populates="users"
+    )
+    notes = relationship("Note", back_populates="user")
+
+
+class GroupMember(BaseModel):
+    group_id = Column(String, ForeignKey("group.id"))
+    user_id = Column(String, ForeignKey("user.id"))
+    role = Column(String, default="owner")
