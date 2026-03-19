c2c361ba59947d0acdcc2be6dad8aea710fbc9dc
jeonghoonyu <moning02004@naver.com>
2026-03-19 16:09:55 +0900

    feat: frontend 노트 리스트 수정

--- frontend/components/card.tsx
+++ frontend/components/card.tsx
@@ -1,12 +1,21 @@
-import {RiExternalLinkLine} from "react-icons/ri";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import {TemplateMenu} from "@/components/template_menu";
 import {NoteMenu} from "@/components/note_menu";
 import {Badge} from "@/components/badge";
 import {DeletedMenu} from "@/components/deleted_menu";
+import DOMPurify from 'dompurify';
 
-export const Card = ({onClick, hashId, title, content, isPublic, isProtected, created_at, noteMenu, templateMenu, deletedMenu}: {
+export const Card = ({
+                         onClick,
+                         hashId,
+                         title,
+                         content,
+                         isPublic,
+                         isProtected,
+                         created_at,
+                         noteMenu,
+                         templateMenu,
+                         deletedMenu
+                     }: {
     onClick?: React.MouseEventHandler<HTMLDivElement>
     hashId: string,
     title: string,
@@ -19,13 +28,16 @@ export const Card = ({onClick, hashId, title, content, isPublic, isProtected, cr
     templateMenu?: boolean,
     deletedMenu?: boolean,
 }) => {
+    const cleanContent = DOMPurify.sanitize(content);
 
     return (
         <div className={`flex-1 flex flex-col h-[17rem] mx-auto mb-8 group w-[90%]`}>
             <div className="relative bg-white flex-10 overflow-hidden p-3 hover:shadow-lg  hover:border-[#888888]  text-ellipsis rounded border  border-[#afafaf] whitespace-pre-line hover:bg-emerald-50
                             transition-all duration-200 ">
-                {isPublic && <div className="absolute left-3 top-0 bg-white"><Badge text="공유됨" isPublic={true} /></div>}
-                <div className="cursor-pointer h-[100%]" onClick={onClick}>{content}</div>
+                {isPublic && <div className="absolute left-3 top-0 bg-white"><Badge text="공유됨" isPublic={true}/></div>}
+                <div className="cursor-pointer h-[100%]" onClick={onClick}>
+                    <div dangerouslySetInnerHTML={{ __html: cleanContent }} />
+                </div>
                 {
                     noteMenu && <NoteMenu noteId={hashId} canDelete={!isProtected}/>
                 }
