df67ed9152a7024ee970239bf34e6d618612148e
jeonghoonyu <moning02004@naver.com>
2026-03-25 21:38:43 +0900

    feat: frontend 코드블럭 커스터마이징

--- frontend/lib/create_editor.tsx
+++ frontend/lib/create_editor.tsx
@@ -4,16 +4,78 @@ import Placeholder from "@tiptap/extension-placeholder";
 import Image from '@tiptap/extension-image'
 import {Dropcursor} from "@tiptap/extensions";
 import TextAlign from "@tiptap/extension-text-align";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import {createLowlight} from "lowlight";
+import javascript from 'highlight.js/lib/languages/javascript'
+import python from 'highlight.js/lib/languages/python'
+import 'highlight.js/styles/atom-one-dark.css'
+
+export const CustomCodeBlock = CodeBlockLowlight.extend({
+    addKeyboardShortcuts() {
+        return {
+            Tab: ({editor}) => {
+                if (editor.isActive('codeBlock')) {
+                    editor.commands.insertContent('  ') // 공백 2칸
+                    return true
+                }
+                return false
+            },
+
+            'Shift-Tab': ({editor}) => {
+                if (editor.isActive('codeBlock')) {
+                    const {state, dispatch} = editor.view
+                    const {from, to} = state.selection
+
+                    const text = state.doc.textBetween(from - 2, from)
+                    if (text === '  ') {
+                        dispatch(
+                            state.tr.delete(from - 2, from)
+                        )
+                    }
+
+                    return true
+                }
+                return false
+            },
+        }
+    },
+
+    renderHTML({node, HTMLAttributes}) {
+        const language = node.attrs.language || 'text'
+
+        return [
+            'pre',
+            {
+                'data-language': language,
+            },
+            [
+                'code',
+                {
+                    ...HTMLAttributes,
+                    class: `language-${language}`,
+                },
+                0,
+            ],
+        ]
+    },
+})
+
 
 export function useEditorInstance({initialContent, setContent}: {
     initialContent: string,
     setContent: (value: string) => void
 }) {
+    const lowlight = createLowlight()
+    lowlight.register('javascript', javascript)
+    lowlight.register('python', python)
+
 
     return useEditor({
         immediatelyRender: false,
         extensions: [
-            StarterKit,
+            StarterKit.configure({
+                codeBlock: false,
+            }),
             Image.configure({
                 resize: {
                     enabled: true,
@@ -27,6 +89,10 @@ export function useEditorInstance({initialContent, setContent}: {
             Placeholder.configure({
                 placeholder: "내용을 입력하세요...",
             }),
+            CustomCodeBlock.configure({
+                lowlight,
+            }),
+
         ],
         content: initialContent,
         editorProps: {
