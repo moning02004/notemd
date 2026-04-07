38dc96a7f30d5d6def5c111cd3b47afe4952ddb4
jeonghoonyu <moning02004@naver.com>
2026-04-07 22:46:15 +0900

    fix: 기본 sort 값 변경

--- backend/app/modules/note/interfaces/schemas.py
+++ backend/app/modules/note/interfaces/schemas.py
@@ -9,13 +9,13 @@ class QueryParams(BaseModel):
     keyword: str | None = Query(None, min_length=2)
     limit: int = Query(10, ge=1, le=100)
     tag: str | None = Query(None)
-    sort: str | None = Query("desc")
+    sort: str | None = Query("-updated_at")
     is_deleted: int = Query(0)
 
 
 def get_query_params(keyword: str = Query(None),
                      limit: int = Query(10),
-                     sort: str = Query("desc"),
+                     sort: str = Query("-updated_at"),
                      tag: str = Query(None),
                      is_deleted: int = Query(0)) -> QueryParams:
     return QueryParams(keyword=keyword, limit=limit, is_deleted=is_deleted, tag=tag, sort=sort)
